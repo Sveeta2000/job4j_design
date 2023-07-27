0feb661a46c706167af1a6ba08f55b9d53c44a30
sveet <sveetlana2000@mail.ru>
2023-07-27 15:42:04 +0300

    6. Читаем файл конфигурации [#858  [#858]]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.matches(".+=.+")) {
+                    int index = line.indexOf('=');
+                    values.put(line.substring(0, index), line.substring(index + 1));
+                }
+            }
+            if (values.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Svetlana Kashina");
+    }
+
+    @Test
+    void whenPairWithCommentAndEmptyLine() {
+        String path = "./data/with_comments.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("key")).isEqualTo("value");
+        assertThat(config.value("key2")).isEqualTo("value2");
+    }
+
+    @Test
+    void whenNoValueThrowException() {
+        String path = "./data/no_value.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNoKeyThrowException() {
+        String path = "./data/no_key.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNoKeyAndValueThrowException() {
+        String path = "./data/no_key_and_value.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
