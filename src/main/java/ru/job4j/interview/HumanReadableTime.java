8f0c342885ea5cdea290d6fa6afb5b34c62c7d9b
sveet <sveetlana2000@mail.ru>
2023-08-02 18:55:51 +0300

    Two katas for codewars

---
+++ src/main/java/ru/job4j/interview/HumanReadableTime.java
@@ -0,0 +1,13 @@
+package ru.job4j.interview;
+
+public class HumanReadableTime {
+    public static String makeReadable(int seconds) {
+        int hours = seconds / 3600;
+        seconds = seconds - (hours * 3600);
+        int minutes = seconds / 60;
+        seconds = seconds - (minutes * 60);
+        return (hours < 10 ? "0" + hours : hours) + ":"
+                + (minutes < 10 ? "0" + minutes : minutes) + ":"
+                + (seconds < 10 ? "0" + seconds : seconds);
+    }
+}
---
+++ src/main/java/ru/job4j/interview/PigLatin.java
@@ -0,0 +1,16 @@
+package ru.job4j.interview;
+
+public class PigLatin {
+    public static String pigIt(String str) {
+        StringBuilder result = new StringBuilder();
+        String[] words = str.split(" ");
+        for (String word : words) {
+            if (word.matches("\\w+")) {
+                result.append(word.substring(1)).append(word.charAt(0)).append("ay ");
+            } else {
+                result.append(word);
+            }
+        }
+        return result.toString().trim();
+    }
+}
