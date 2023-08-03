3601cf23e4a685b2ba64209a613a4ea19640766c
sveet <sveetlana2000@mail.ru>
2023-08-03 16:08:21 +0300

    10. Сканирование файловой системы [#106929  [#106929]]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+
+    List<Path> paths = new ArrayList<>();
+
+    Predicate<Path> condition;
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (condition.test(file)) {
+            paths.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
