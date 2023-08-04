2c337f48692c71d6135a475d31aa9f101864ac55
sveet <sveetlana2000@mail.ru>
2023-08-04 13:29:33 +0300

    12. Валидация параметров запуска [#246865  [#246865]]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Check if you have provided both the root folder and the file extension");
+        }
+        validate(args[0], args[1]);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validate(String path, String extension) {
+        if (!Files.exists(Path.of(path))) {
+            throw new IllegalArgumentException(String.format("The folder %s doesn't exist", path));
+        }
+        if (!extension.matches("\\..+")) {
+            throw new IllegalArgumentException(String.format("The extension format is not correct : %s", extension));
+        }
     }
 }
