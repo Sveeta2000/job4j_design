fe3e3dbfdae9791a1ddcff5fd56b6de0f5742004
sveet <sveetlana2000@mail.ru>
2023-09-28 17:42:40 +0300

    Direction Reduction

---
+++ src/main/java/ru/job4j/interview/DirReduction.java
@@ -0,0 +1,40 @@
+package ru.job4j.interview;
+
+import java.util.Stack;
+
+public class DirReduction {
+    public static String[] dirReduc(String[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new String[] {};
+        }
+        Stack<String> stack = new Stack<>();
+        for (String s : arr) {
+            stack.push(s);
+            if (stack.size() >= 2) {
+                String last = stack.pop();
+                String prev = stack.peek();
+                if (findOpposite(last).equals(prev)) {
+                    stack.pop();
+                } else {
+                    stack.push(last);
+                }
+            }
+        }
+        return stack.toArray(new String[0]);
+    }
+
+    private static String findOpposite(String str) {
+        return switch (str) {
+            case "NORTH" -> "SOUTH";
+            case "SOUTH" -> "NORTH";
+            case "EAST" -> "WEST";
+            case "WEST" -> "EAST";
+            default -> "";
+        };
+    }
+
+    public static void main(String[] args) {
+        String[] a = new String[] {"a", "b"};
+        System.out.println(String.join(" ", a));
+    }
+}
---
+++ src/test/java/ru/job4j/interview/DirReductionTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.interview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class DirReductionTest {
+    @Test
+    public void testSimpleDirReduc() {
+        assertArrayEquals(new String[]{"WEST"},
+                DirReduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
+        assertArrayEquals(new String[]{},
+                DirReduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"}));
+    }
+}
