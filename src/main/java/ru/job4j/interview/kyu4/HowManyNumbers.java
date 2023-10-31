54693b2af8504940f75a6c2ca5f5404069dd5a15
sveet <sveetlana2000@mail.ru>
2023-10-31 19:02:31 +0300

    1 kata from codewars

--- src/main/java/ru/job4j/interview/kyu4/HowManyNumbers.java
+++ src/main/java/ru/job4j/interview/kyu4/HowManyNumbers.java
@@ -12,8 +12,38 @@ public class HowManyNumbers {
             return new ArrayList<>();
         }
         biggest = findBiggest(sumDigits, numDigits);
+        smallest = findSmallest(sumDigits, numDigits);
         Set<Long> longSet = generateNumbers(sumDigits, numDigits);
-        return List.of((long) longSet.size(), Collections.min(longSet), biggest);
+        return List.of((long) longSet.size(), smallest, biggest);
+    }
+
+    private static Set<Long> generateNumbers(int sumDigits, int numDigits) {
+        Set<Long> set = new HashSet<>();
+        long num = smallest;
+        while (num <= biggest) {
+            if (sumDigits == countSum(num)) {
+                if (isConsec(num)) {
+                    set.add(num);
+                    num += 9;
+                } else {
+                    num += 9;
+                    //num = skipNonConsecutive(num, numDigits);
+                }
+            } else {
+                num += 9;
+            }
+        }
+        return set;
+    }
+
+    private static boolean isConsec(long l) {
+        while (l != 0) {
+            if ((l % 10) < ((l / 10) % 10)) {
+                return false;
+            }
+            l /= 10;
+        }
+        return true;
     }
 
     public static long findBiggest(int sumDigits, int numDigits) {
@@ -37,6 +67,29 @@ public class HowManyNumbers {
         return Long.parseLong(number.toString());
     }
 
+    private static long findSmallest(int sumDigits, int numDigits) {
+        if (sumDigits / numDigits > 9) {
+            return -1;
+        }
+        StringBuilder number = new StringBuilder("1".repeat(numDigits));
+        int leftover = sumDigits - numDigits;
+        int index = numDigits - 1;
+        while (leftover > 0) {
+            if (leftover >= 8) {
+                number.setCharAt(index, '9');
+                leftover -= 8;
+            } else {
+                number.setCharAt(index, (char) ('1' + leftover));
+                leftover -= leftover;
+            }
+            index--;
+            if (index == -1) {
+                index = numDigits - 1;
+            }
+        }
+        return Long.parseLong(number.toString());
+    }
+
     private static int countSum(long num) {
         int sum = 0;
         while (num > 0) {
@@ -46,54 +99,26 @@ public class HowManyNumbers {
         return sum;
     }
 
-    private static Set<Long> generateNumbers(int sumDigits, int numDigits) {
-        Set<Long> set = new HashSet<>();
-        long num = 0;
-        long val = 1;
-        for (int i = 0; i < numDigits; i++) {
-            num += val;
-            val *= 10;
-        }
-        while (num <= biggest) {
-            if (sumDigits == countSum(num)) {
-                if (isConsecutive(num)) {
-                    set.add(num);
-                    num += 9;
-                } else {
-                    num = skipNonConsecutive(num, numDigits);
-                }
-            } else {
-                num++;
-            }
-        }
-        return set;
-    }
-
-    private static boolean isConsecutive(long number) {
-        String num = String.valueOf(number);
-        for (int i = 0; i < num.length() - 1; i++) {
-            if (num.charAt(i) > num.charAt(i + 1)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static long skipNonConsecutive(long number, int numDigits) {
+    private static long skipNonConsecutive(long number, int numDigits) { //redo
         String n = Long.toString(number).substring(0, 1);
         int next = Integer.parseInt(n) + 1;
         return Long.parseLong(String.valueOf(next).repeat(numDigits));
     }
 
     public static void main(String[] args) {
-       System.out.println(findBiggest(35, 6));
-       System.out.println(findBiggest(10, 3));
-       System.out.println(findBiggest(27, 3));
-       System.out.println(findBiggest(84, 4));
+        System.out.println(findBiggest(35, 6));
+        System.out.println(findBiggest(10, 3));
+        System.out.println(findBiggest(27, 3));
+        System.out.println(findBiggest(84, 4));
         System.out.println(findBiggest(36, 4));
         System.out.println(findBiggest(20, 4));
         System.out.println(findBiggest(37, 5));
         System.out.println(findBiggest(22, 8));
+
+        System.out.println(findSmallest(35, 6));
+        System.out.println(findSmallest(10, 3));
+        System.out.println(findSmallest(27, 3));
+        System.out.println(findSmallest(84, 4));
     }
 
 }
--- src/test/java/ru/job4j/interview/kyu4/HowManyNumbersTest.java
+++ src/test/java/ru/job4j/interview/kyu4/HowManyNumbersTest.java
@@ -16,4 +16,12 @@ class HowManyNumbersTest {
         assertEquals(new ArrayList<Long>(),                 HowManyNumbers.findAll(84, 4));
         assertEquals(Arrays.asList(123L, 116999L, 566666L), HowManyNumbers.findAll(35, 6));
     }
+
+    @Test
+    public void largeTests() {
+        assertEquals(Arrays.asList(519L, 11129999L, 55555556L), HowManyNumbers.findAll(41, 8));
+        assertEquals(Arrays.asList(902L, 111169999L, 555555556L),  HowManyNumbers.findAll(46, 9));
+        assertEquals(Arrays.asList(319L, 11399999L, 66666677L), HowManyNumbers.findAll(50, 8));
+        assertEquals(Arrays.asList(194L, 11799999L, 66777777L), HowManyNumbers.findAll(54, 8));
+    }
 }
