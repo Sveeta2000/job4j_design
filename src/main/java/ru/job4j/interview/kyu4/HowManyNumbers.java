f925828a56acf6c39c3974982bbe430bddb65883
sveet <sveetlana2000@mail.ru>
2023-11-01 15:40:11 +0300

    How Many Numbers 4 kyu super challenging kata

--- src/main/java/ru/job4j/interview/kyu4/HowManyNumbers.java
+++ src/main/java/ru/job4j/interview/kyu4/HowManyNumbers.java
@@ -4,121 +4,33 @@ import java.util.*;
 
 public class HowManyNumbers {
 
-    private static long biggest;
-    private static long smallest;
-
     public static List<Long> findAll(final int sumDigits, final int numDigits) {
         if (sumDigits > numDigits * 9 || sumDigits < numDigits) {
             return new ArrayList<>();
         }
-        biggest = findBiggest(sumDigits, numDigits);
-        smallest = findSmallest(sumDigits, numDigits);
-        Set<Long> longSet = generateNumbers(sumDigits, numDigits);
-        return List.of((long) longSet.size(), smallest, biggest);
-    }
-
-    private static Set<Long> generateNumbers(int sumDigits, int numDigits) {
         Set<Long> set = new HashSet<>();
-        long num = smallest;
-        while (num <= biggest) {
-            if (sumDigits == countSum(num)) {
-                if (isConsec(num)) {
-                    set.add(num);
-                    num += 9;
-                } else {
-                    num += 9;
-                    //num = skipNonConsecutive(num, numDigits);
-                }
-            } else {
-                num += 9;
+        char[] out = new char[numDigits];
+        for (int i = 1; i <= 9; i++) {
+            out[0] = (char) (i + '0');
+            findDigits(numDigits, sumDigits - i, out, 1, i, set);
+        }
+        return List.of((long) set.size(), Collections.min(set), Collections.max(set));
+    }
+
+    static void findDigits(int numDigits, int sumDigits, char[] out,
+                           int index, int startingDigit, Set<Long> set) {
+        if (index > numDigits || sumDigits < 0) {
+            return;
+        }
+        if (index == numDigits) {
+            if (sumDigits == 0) {
+                set.add(Long.parseLong(new String(out)));
             }
+            return;
         }
-        return set;
-    }
-
-    private static boolean isConsec(long l) {
-        while (l != 0) {
-            if ((l % 10) < ((l / 10) % 10)) {
-                return false;
-            }
-            l /= 10;
+        for (int i = startingDigit; i <= 9; i++) {
+            out[index] = (char) (i + '0');
+            findDigits(numDigits, sumDigits - i, out, index + 1, i, set);
         }
-        return true;
     }
-
-    public static long findBiggest(int sumDigits, int numDigits) {
-        int digit = sumDigits / numDigits;
-        if (digit > 9) {
-            return -1;
-        }
-        StringBuilder number = new StringBuilder(String.valueOf(digit).repeat(numDigits));
-        int leftover = sumDigits % numDigits;
-        int index = numDigits - 1;
-        while (leftover > 0) {
-            if (number.charAt(index) < '9') {
-                number.setCharAt(index, (char) (number.charAt(index) + 1));
-                leftover--;
-            }
-            index--;
-            if (index == -1) {
-                index = numDigits - 1;
-            }
-        }
-        return Long.parseLong(number.toString());
-    }
-
-    private static long findSmallest(int sumDigits, int numDigits) {
-        if (sumDigits / numDigits > 9) {
-            return -1;
-        }
-        StringBuilder number = new StringBuilder("1".repeat(numDigits));
-        int leftover = sumDigits - numDigits;
-        int index = numDigits - 1;
-        while (leftover > 0) {
-            if (leftover >= 8) {
-                number.setCharAt(index, '9');
-                leftover -= 8;
-            } else {
-                number.setCharAt(index, (char) ('1' + leftover));
-                leftover -= leftover;
-            }
-            index--;
-            if (index == -1) {
-                index = numDigits - 1;
-            }
-        }
-        return Long.parseLong(number.toString());
-    }
-
-    private static int countSum(long num) {
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-        return sum;
-    }
-
-    private static long skipNonConsecutive(long number, int numDigits) { //redo
-        String n = Long.toString(number).substring(0, 1);
-        int next = Integer.parseInt(n) + 1;
-        return Long.parseLong(String.valueOf(next).repeat(numDigits));
-    }
-
-    public static void main(String[] args) {
-        System.out.println(findBiggest(35, 6));
-        System.out.println(findBiggest(10, 3));
-        System.out.println(findBiggest(27, 3));
-        System.out.println(findBiggest(84, 4));
-        System.out.println(findBiggest(36, 4));
-        System.out.println(findBiggest(20, 4));
-        System.out.println(findBiggest(37, 5));
-        System.out.println(findBiggest(22, 8));
-
-        System.out.println(findSmallest(35, 6));
-        System.out.println(findSmallest(10, 3));
-        System.out.println(findSmallest(27, 3));
-        System.out.println(findSmallest(84, 4));
-    }
-
 }
