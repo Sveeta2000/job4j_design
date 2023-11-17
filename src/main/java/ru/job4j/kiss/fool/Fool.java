6de845836d5c6c867a7d3ab174eba576416d3f33
sveet <sveetlana2000@mail.ru>
2023-11-17 19:39:42 +0300

    2. Принципы Kiss, Dry и Yagni [#238813 #460667]

--- src/main/java/ru/job4j/kiss/fool/Fool.java
+++ src/main/java/ru/job4j/kiss/fool/Fool.java
@@ -3,68 +3,26 @@ package ru.job4j.kiss.fool;
 import java.util.Scanner;
 
 public class Fool {
-    private static int number = 1;
-    private static final String FIZZ_BUZZ = "FizzBuzz";
-    private static final String FIZZ = "Fizz";
-    private static final String BUZZ = "Buzz";
 
     public static void main(String[] args) {
         System.out.println("Игра FizzBuzz.");
+        int number = 1;
         Scanner scanner = new Scanner(System.in);
         while (number < 100) {
-            computerTurn(number);
-            number++;
+            System.out.println(checkAnswer(number++));
             String answer = scanner.nextLine();
-            playerTurn(answer);
+            if (!answer.equals(checkAnswer(number))) {
+                System.out.println("Ошибка. Начинай снова.");
+                number = 0;
+            }
             number++;
         }
     }
 
-    public static void computerTurn(int currNumber) {
-        if (isDivBy3And5(currNumber)) {
-            System.out.println(FIZZ_BUZZ);
-        } else if (isDivBy3(currNumber)) {
-            System.out.println(FIZZ);
-        } else if (isDivBy5(currNumber)) {
-            System.out.println(BUZZ);
-        } else {
-            System.out.println(currNumber);
-        }
+    public static String checkAnswer(int number) {
+        String out = String.format("%s%s",
+                number % 3 == 0 ? "Fizz" : "",
+                number % 5 == 0 ? "Buzz" : "");
+        return out.isEmpty() ? String.valueOf(number) : out;
     }
-
-    public static void playerTurn(String answer) {
-        validateAnswer(answer);
-        if (isDivBy3And5(number) && !FIZZ_BUZZ.equals(answer)) {
-            startAgain();
-        } else if (isDivBy3(number) && !FIZZ.equals(answer)) {
-            startAgain();
-        } else if (isDivBy5(number) && !BUZZ.equals(answer)) {
-            startAgain();
-        }
-    }
-
-    public static void validateAnswer(String answer) {
-        if (!String.valueOf(number).equals(answer) && !FIZZ.equals(answer)
-                && !BUZZ.equals(answer) && !FIZZ_BUZZ.equals(answer)) {
-            startAgain();
-        }
-    }
-
-    private static void startAgain() {
-        System.out.println("Ошибка. Начинай снова.");
-        number = 0;
-    }
-
-    public static boolean isDivBy3And5(int currNumber) {
-        return isDivBy5(currNumber) && isDivBy3(currNumber);
-    }
-
-    public static boolean isDivBy5(int currNumber) {
-        return currNumber % 5 == 0;
-    }
-
-    public static boolean isDivBy3(int currNumber) {
-        return currNumber % 3 == 0;
-    }
-
 }
--- src/test/java/ru/job4j/kiss/fool/FoolTest.java
+++ src/test/java/ru/job4j/kiss/fool/FoolTest.java
@@ -7,32 +7,26 @@ import static org.assertj.core.api.Assertions.*;
 class FoolTest {
 
     @Test
-    void when15IsDivBy3And5ThenTrue() {
-        assertThat(Fool.isDivBy3And5(15)).isTrue();
+    void testFizz() {
+        assertThat(Fool.checkAnswer(18)).isEqualTo("Fizz");
+        assertThat(Fool.checkAnswer(99)).isEqualTo("Fizz");
     }
 
     @Test
-    void when20IsDivBy5ThenTrue() {
-        assertThat(Fool.isDivBy5(20)).isTrue();
+    void testBuzz() {
+        assertThat(Fool.checkAnswer(25)).isEqualTo("Buzz");
+        assertThat(Fool.checkAnswer(50)).isEqualTo("Buzz");
     }
 
     @Test
-    void when18IsDivBy3ThenTrue() {
-        assertThat(Fool.isDivBy3(18)).isTrue();
+    void testFizzBuzz() {
+        assertThat(Fool.checkAnswer(15)).isEqualTo("FizzBuzz");
+        assertThat(Fool.checkAnswer(30)).isEqualTo("FizzBuzz");
     }
 
     @Test
-    void when18IsDivBy3And5ThenFalse() {
-        assertThat(Fool.isDivBy3And5(18)).isFalse();
-    }
-
-    @Test
-    void when21IsDivBy5ThenFalse() {
-        assertThat(Fool.isDivBy5(21)).isFalse();
-    }
-
-    @Test
-    void when20IsDivBy3ThenFalse() {
-        assertThat(Fool.isDivBy3(20)).isFalse();
+    void testNumbers() {
+        assertThat(Fool.checkAnswer(16)).isEqualTo("16");
+        assertThat(Fool.checkAnswer(31)).isEqualTo("31");
     }
 }
