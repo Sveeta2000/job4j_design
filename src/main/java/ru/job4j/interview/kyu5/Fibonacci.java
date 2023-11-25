94ce71f454d8192cb4610da717cb2ea99aa79082
sveet <sveetlana2000@mail.ru>
2023-11-26 01:31:53 +0300

    Fibonacci problem 2 ways to solve

---
+++ src/main/java/ru/job4j/interview/kyu5/Fibonacci.java
@@ -0,0 +1,29 @@
+package ru.job4j.interview.kyu5;
+
+public class Fibonacci {
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public int fib2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+    }
+}
