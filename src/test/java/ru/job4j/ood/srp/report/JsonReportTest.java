ca4309c7c243ed61754208cd84780e6b0903d84a
sveet <sveetlana2000@mail.ru>
2023-11-27 20:42:58 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851 #462825]

--- src/test/java/ru/job4j/ood/srp/report/JsonReportTest.java
+++ src/test/java/ru/job4j/ood/srp/report/JsonReportTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.*;
 class JsonReportTest {
 
     @Test
-    void whenGenerateJsonReport() {
+    void whenGenerateJsonReportOnePerson() {
         MemStore store1 = new MemStore();
         Calendar calendar = Calendar.getInstance();
         DateTimeParser<Calendar> parser = new ReportDateTimeParser();
@@ -27,6 +27,31 @@ class JsonReportTest {
               +  "    \"salary\": 100.0\n"
               +  "  }\n"
               +  "]";
+        assertThat(j.generate(employee -> true)).isEqualTo(expected);
+    }
+
+    @Test
+    void whenGenerateJsonReportTwoPeople() {
+        MemStore store1 = new MemStore();
+        Calendar calendar = Calendar.getInstance();
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        store1.add(new Employee("Ivan", calendar, calendar, 100));
+        store1.add(new Employee("Anna", calendar, calendar, 200));
+        JsonReport j = new JsonReport(store1, new ReportDateTimeParser());
+        String expected = "[\n"
+                + "  {\n"
+                + "    \"fired\": \"" + parser.parse(calendar) + "\",\n"
+                + "    \"name\": \"Ivan\",\n"
+                + "    \"hired\": \"" + parser.parse(calendar) + "\",\n"
+                + "    \"salary\": 100.0\n"
+                + "  },\n"
+                + "  {\n"
+                + "    \"fired\": \"" + parser.parse(calendar) + "\",\n"
+                + "    \"name\": \"Anna\",\n"
+                + "    \"hired\": \"" + parser.parse(calendar) + "\",\n"
+                + "    \"salary\": 200.0\n"
+                + "  }\n"
+                + "]";
         assertThat(j.generate(employee -> true)).isEqualTo(expected);
     }
 }
